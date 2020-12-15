b8e046859136709be2a3db69f9cafdea45d1e75b
MuriloAcsonov <murilloacsonovv@hotmail.com>
2020-12-14 21:08:44 -0300

    update

--- php/contact.php
+++ php/contact.php
@@ -2,14 +2,7 @@
 
 if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['subject']) && isset($_POST['message']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
 
-  echo 'console.log("entrou")'
-
-  $test = "/(content-type|bcc:|cc:|to:)/i";
-  foreach ( $_POST as $key => $val ) {
-    if ( preg_match( $test, $val ) ) {
-      exit;
-    }
-  }
+  echo 'console.log("entrou")'  
 
 $headers = 'From: ' . $_POST["name"] . '<' . $_POST["email"] . '>' . "\r\n" .
     'Reply-To: ' . $_POST["email"] . "\r\n" .
